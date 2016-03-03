608aea3ec126001a8ee2c97157114bcf409ebd80
Nicholas Amorim <nicholas@alienretro.com>
2016-03-04 01:39:03 +0200

    Documented widget templatetag.

--- chato/utils/django/templatetags/chato_client.py
+++ chato/utils/django/templatetags/chato_client.py
@@ -1,4 +1,3 @@
-# import os
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -7,6 +6,15 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def chato_widget(context, include_css=True, include_jquery=True):
+    """Returns the HTML of the Chat Widget.
+
+    :param include_css: Defauls to True. Set to False if you want complete
+    control over the styling.
+    :param include_jquery: Defaults to True. If False, we will not add jQuery
+    on the template.
+
+    :returns: HTML string with the chat widget.
+    """
     chat_html = template.loader.get_template('chat.html')
     context['include_css'] = include_css
     context['include_jquery'] = include_jquery
